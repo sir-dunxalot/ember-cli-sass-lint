9416c263b0d0dea0a2b0903fba6891fd8f578a9a
Duncan Graham Walker <walkerdu@bc.edu>
2016-03-14 21:15:22 -0400

    Linting for addon's stylesheets

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var SassLinter = require('broccoli-sass-lint');
 var mergeTrees = require('broccoli-merge-trees');
+var fs = require('fs');
 
 module.exports = {
   name: 'ember-cli-sass-lint',
@@ -17,9 +18,32 @@ module.exports = {
   },
 
   lintTree: function(type, tree) {
-    var formattedResults, mergedTrees, results;
+    var mergedTrees;
 
-    if (type === 'app') {
+    if (type === 'addon') {
+      var treePaths = this.registry.app.treePaths;
+      var paths = [];
+
+      ['addon-styles', 'styles'].forEach(function(subtree) {
+        var path = treePaths[subtree];
+
+        /* Use fs.stat to check if the styles dir exists in
+        addon-name/addon/styles and addon-name/app/style */
+
+        fs.stat(path, function(error, stats) {
+          if (!error && stats.isDirectory()) {
+            paths.push(path);
+          }
+        });
+      });
+
+      mergedTrees = mergeTrees(paths);
+
+      return new SassLinter(mergedTrees, this.sassLintOptions);
+    } else if (type === 'app') {
+
+      /* Lints app/styles and addon/tests/dummy/app/styles */
+
       mergedTrees = mergeTrees([this.app.trees.styles]);
 
       return new SassLinter(mergedTrees, this.sassLintOptions);
