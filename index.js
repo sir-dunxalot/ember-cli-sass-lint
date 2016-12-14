e161da27c358dd1ac05903d5d4b6597e659c0857
Robert Wagner <rwwagner90@gmail.com>
2016-12-14 10:54:19 -0500

    Rebase and add back addon logic

--- index.js
+++ index.js
@@ -20,7 +20,28 @@ module.exports = {
   lintTree: function(type, tree) {
     var mergedTrees;
 
-    if (type === 'app') {
+    if (type === 'addon') {
+      var treePaths = this.registry.app.treePaths;
+      var paths = [];
+
+      ['addon-styles', 'styles'].forEach(function(subtree) {
+        var path = treePaths[subtree];
+
+        /* Use fs.stat to check if the styles dir exists in
+         addon-name/addon/styles and addon-name/app/style */
+
+        fs.stat(path, function(error, stats) {
+          if (!error && stats.isDirectory()) {
+            paths.push(path);
+          }
+        });
+      });
+
+      mergedTrees = mergeTrees(paths);
+
+      return new SassLinter(mergedTrees, this.sassLintOptions);
+    }
+    else if (type === 'app') {
       mergedTrees = mergeTrees([this.app.trees.styles]);
 
       return new SassLinter(mergedTrees, this.sassLintOptions);
